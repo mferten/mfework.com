535ee517ffe8c95ff66f00e23718fc6c8fb93981
Mehmet F Erten <mferten@mfeweb.com>
2018-06-02 13:57:59 -0400

    commit all

---
+++ resources/views/myWeb/frontPage.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@push('header')
+<!-- place holder -->
+        <header>
+            <h1>M.F.E is a Vegan. Why?</h1>
+            <figure>
+                <a href="images/thisIsMyMovie.png" target="_blank">
+                    <img class="img14 aPng" src="images/Design5_Erten-MehmetBlue.png"
+                        alt="CSCI E-50 Introduction to Communication Design Project in Adobe Illustrator"/>
+                </a>
+            </figure>
+        </header>
+@endpush
+@push('module')
+<!-- place holder -->
+        <main>
+            <h2 class='harvard'>Here are Netflix Documentaries</h2>
+            <figure>
+                <figcaption><p class='harvard'>Programming</p></figcaption>
+                <a href="images/programmingcertificate.png" target="_blank">
+                    <img class="img10 aPng" src="images/programmingcertificate.png"
+                        alt="MFE Harvard FAS DCE HES Programming Certificate"/>
+                </a>
+            </figure>
+            <h2 class='harvard'>Here are the Questions</h2>
+            <figure>
+                <figcaption><p class='harvard'>Web Technologies</p></figcaption>
+                <a href="images/webtechnologiescertificate.png" target="_blank">
+                    <img class="img10 aPng" src="images/webtechnologiescertificate.png"
+                        alt="MFE Harvard FAS DCE HES Web Technologies Certificate"/>
+                </a>
+            </figure>
+        </main>
+@endpush
+
+@push('jsFiles')
+<!-- place holder -->
+        <script src="js/mfeweb.js"></script>
+@endpush
