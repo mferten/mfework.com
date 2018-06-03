3275d55b78c4110d1d2fb145511201e45bf4d92f
Mehmet F Erten <mferten@mfeweb.com>
2018-06-03 09:16:07 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -12,35 +12,35 @@
             <figure>
                 <figcaption><p class='titleColorTwo'>What the Health</p></figcaption>
                 <a href="http://www.whatthehealthfilm.com/" target="_blank">
-                    <img class="img10 aPng" src="images/whatTheHealth.jpg"
+                    <img class="img8 aPng" src="images/whatTheHealth.jpg"
                     alt="What the Health: Direct or Netflix"/>
                 </a>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>The Magic Pill</p></figcaption>
                 <a href="https://www.netflix.com/title/80238655" target="_blank">
-                    <img class="img10 aPng" src="images/theMagicPill.jpg"
+                    <img class="img8 aPng" src="images/theMagicPill.jpg"
                     alt="The Magic Pill: Netflix"/>
                 </a>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>The C Word</p></figcaption>
                 <a href="http://thecwordmovie.com/" target="_blank">
-                    <img class="img10 aPng" src="images/theCWord.jpg"
+                    <img class="img8 aPng" src="images/theCWord.jpg"
                     alt="The C Word: Direct or Netflix"/>
                 </a>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>In Search of Balance</p></figcaption>
                 <a href="https://www.netflix.com/title/80164396" target="_blank">
-                    <img class="img10 aPng" src="images/inSearchOfBalance.jpg"
+                    <img class="img8 aPng" src="images/inSearchOfBalance.jpg"
                     alt="In Search of Balance Netflix"/>
                 </a>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>Evolution of Diet</p></figcaption>
                 <a href="https://www.nationalgeographic.com/foodfeatures/evolution-of-diet/" target="_blank">
-                    <img class="img10 aPng" src="images/evolutionOfDiet.jpg"
+                    <img class="img8 aPng" src="images/evolutionOfDiet.jpg"
                     alt="Evolution of Diet National Geographic"/>
                 </a>
             </figure>
