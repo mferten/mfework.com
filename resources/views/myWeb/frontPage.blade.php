4966f7e8ab8dc8875f0cdcd49c4022371e163b88
Mehmet F Erten <mferten@mfeweb.com>
2018-06-03 17:11:59 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -2,19 +2,22 @@
 @push('header')
 <!-- place holder -->
         <header>
-            <h1>M.F.E is a Vegan. Is it HumanThen Why?</h1>
+            <h1>M.F.E is a Vegan for Now. Why?</h1>
         </header>
 @endpush
 @push('module')
 <!-- place holder -->
         <main>
-            <h2 class='harvard'>Here are the Documentaries</h2>
+            <h2 class='harvard'>Here are my Sources</h2>
             <figure>
                 <figcaption><p class='titleColorTwo'>What the Health</p></figcaption>
                 <a href="http://www.whatthehealthfilm.com/" target="_blank">
                     <img class="img8 aPng" src="images/whatTheHealth.jpg"
                     alt="What the Health: Direct or Netflix"/>
                 </a>
+                <figcaption><p class='titleColorTwo'>Whole Foods</p></figcaption>
+                <figcaption><p class='harvard'>No Meat (& Products)</p></figcaption>
+                <figcaption><p class='titleColorTwo'>No Milk (& Products)</p></figcaption>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>The Magic Pill</p></figcaption>
@@ -22,6 +25,9 @@
                     <img class="img8 aPng" src="images/theMagicPill.jpg"
                     alt="The Magic Pill: Netflix"/>
                 </a>
+                <figcaption><p class='titleColorTwo'>Whole Foods</p></figcaption>
+                <figcaption><p class='harvard'>No Milk (& Products)</p></figcaption>
+                <figcaption><p class='titleColorTwo'>Meat (& Products)</p></figcaption>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>The C Word</p></figcaption>
@@ -29,6 +35,9 @@
                     <img class="img8 aPng" src="images/theCWord.jpg"
                     alt="The C Word: Direct or Netflix"/>
                 </a>
+                <figcaption><p class='titleColorTwo'>Healthy Diet</p></figcaption>
+                <figcaption><p class='harvard'>Excercise (No Stress)</p></figcaption>
+                <figcaption><p class='titleColorTwo'>No Toxin</p></figcaption>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>In Search of Balance</p></figcaption>
@@ -36,21 +45,34 @@
                     <img class="img8 aPng" src="images/inSearchOfBalance.jpg"
                     alt="In Search of Balance Netflix"/>
                 </a>
+                <figcaption><p class='titleColorTwo'>Whole Foods</p></figcaption>
+                <figcaption><p class='harvard'>Nature</p></figcaption>
+                <figcaption><p class='titleColorTwo'>Integration</p></figcaption>
             </figure>
             <figure>
                 <figcaption><p class='titleColorTwo'>Evolution of Diet</p></figcaption>
                 <a href="https://www.nationalgeographic.com/foodfeatures/evolution-of-diet/" target="_blank">
-                    <img class="img8 aPng" src="images/evolutionOfDiet.jpg"
-                    alt="Evolution of Diet National Geographic"/>
+                    <img class="img8 aPng" src="images/evolutionOfDiet.jpg" alt="Humans eat what, where, how?"/>
                 </a>
+                <figcaption><p class='titleColorTwo'>What</p></figcaption>
+                <figcaption><p class='harvard'>Where</p></figcaption>
+                <figcaption><p class='titleColorTwo'>How</p></figcaption>
             </figure>
-            <h2 class='harvard'>Here are the Questions</h2>
+            <h2 class='harvard'>the Questions</h2>
             <figure>
-                <figcaption><p class='harvard'>Web Technologies</p></figcaption>
-                <a href="images/webtechnologiescertificate.png" target="_blank">
-                    <img class="img10 aPng" src="images/webtechnologiescertificate.png"
-                        alt="MFE Harvard FAS DCE HES Web Technologies Certificate"/>
-                </a>
+                <figcaption><p class='titleColorTwo'>Any Milk is harmful</p></figcaption>
+            </figure>
+            <figure>
+                <figcaption><p class='titleColorTwo'>Any Meat is harmful</p></figcaption>
+            </figure>
+            <figure>
+                <figcaption><p class='titleColorTwo'>Any Legume is harmful</p></figcaption>
+            </figure>
+            <figure>
+                <figcaption><p class='titleColorTwo'>Any Sugar is harmful</p></figcaption>
+            </figure>
+            <figure>
+                <figcaption><p class='titleColorTwo'>Any City is harmful</p></figcaption>
             </figure>
         </main>
 @endpush
