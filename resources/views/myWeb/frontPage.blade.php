b62fe0e6a50fe4084288890cc896b92fff7328ff
Mehmet F Erten <mferten@mfeweb.com>
2018-06-03 09:12:56 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -2,7 +2,7 @@
 @push('header')
 <!-- place holder -->
         <header>
-            <h1>M.F.E is a Vegan. Why?</h1>
+            <h1>M.F.E is a Vegan. Is it HumanThen Why?</h1>
         </header>
 @endpush
 @push('module')
@@ -10,31 +10,38 @@
         <main>
             <h2 class='harvard'>Here are the Documentaries</h2>
             <figure>
-                <figcaption><p class='harvard'>What the Health</p></figcaption>
+                <figcaption><p class='titleColorTwo'>What the Health</p></figcaption>
                 <a href="http://www.whatthehealthfilm.com/" target="_blank">
                     <img class="img10 aPng" src="images/whatTheHealth.jpg"
                     alt="What the Health: Direct or Netflix"/>
                 </a>
             </figure>
             <figure>
-                <figcaption><p class='harvard'>The Magic Pill</p></figcaption>
+                <figcaption><p class='titleColorTwo'>The Magic Pill</p></figcaption>
                 <a href="https://www.netflix.com/title/80238655" target="_blank">
                     <img class="img10 aPng" src="images/theMagicPill.jpg"
                     alt="The Magic Pill: Netflix"/>
                 </a>
             </figure>
             <figure>
-                <figcaption><p class='harvard'>The C Word</p></figcaption>
+                <figcaption><p class='titleColorTwo'>The C Word</p></figcaption>
                 <a href="http://thecwordmovie.com/" target="_blank">
                     <img class="img10 aPng" src="images/theCWord.jpg"
                     alt="The C Word: Direct or Netflix"/>
                 </a>
             </figure>
             <figure>
-                <figcaption><p class='harvard'>In Search of Balance</p></figcaption>
+                <figcaption><p class='titleColorTwo'>In Search of Balance</p></figcaption>
                 <a href="https://www.netflix.com/title/80164396" target="_blank">
                     <img class="img10 aPng" src="images/inSearchOfBalance.jpg"
                     alt="In Search of Balance Netflix"/>
+                </a>
+            </figure>
+            <figure>
+                <figcaption><p class='titleColorTwo'>Evolution of Diet</p></figcaption>
+                <a href="https://www.nationalgeographic.com/foodfeatures/evolution-of-diet/" target="_blank">
+                    <img class="img10 aPng" src="images/evolutionOfDiet.jpg"
+                    alt="Evolution of Diet National Geographic"/>
                 </a>
             </figure>
             <h2 class='harvard'>Here are the Questions</h2>
