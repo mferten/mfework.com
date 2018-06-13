09317ec0603eb8622c90061856461b382c9de40d
Mehmet F Erten <mferten@mfeweb.com>
2018-06-12 21:43:59 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -8,57 +8,57 @@
 @push('module')
 <!-- place holder -->
         <main>
-            <h2 class='harvard'><a class="harvard" href="docs/whyMFEIsAVegan.jpg" target="_blank">*** Because ***</a></h2>
             <section id="documentaries">
-            <figure>
-                <figcaption><p class='titleColorTwo'>What the Health</p></figcaption>
-                <a href="http://www.whatthehealthfilm.com/" target="_blank">
-                    <img class="img8 aPng" src="images/whatTheHealth.jpg"
-                    alt="What the Health: Direct or Netflix"/>
-                </a>
-                <p class='titleColorTwo'>Whole Foods</p>
-                <p class='harvard'>No Meat</p>
-                <p class='titleColorTwo'>No Milk</p>
-            </figure>
-            <figure>
-                <figcaption><p class='titleColorTwo'>The Magic Pill</p></figcaption>
-                <a href="https://www.netflix.com/title/80238655" target="_blank">
-                    <img class="img8 aPng" src="images/theMagicPill.jpg"
-                    alt="The Magic Pill: Netflix"/>
-                </a>
-                <p class='titleColorTwo'>Whole Foods</p>
-                <p class='harvard'>No Milk</p>
-                <p class='titleColorTwo'>Meat</p>
-            </figure>
-            <figure>
-                <figcaption><p class='titleColorTwo'>The C Word</p></figcaption>
-                <a href="http://thecwordmovie.com/" target="_blank">
-                    <img class="img8 aPng" src="images/theCWord.jpg"
-                    alt="The C Word: Direct or Netflix"/>
-                </a>
-                <p class='titleColorTwo'>Healthy Diet!</p>
-                <p class='harvard'>Excercise</p>
-                <p class='titleColorTwo'>No Toxin</p>
-            </figure>
-            <figure>
-                <figcaption><p class='titleColorTwo'>In Search of Balance</p></figcaption>
-                <a href="https://www.netflix.com/title/80164396" target="_blank">
-                    <img class="img8 aPng" src="images/inSearchOfBalance.jpg"
-                    alt="In Search of Balance Netflix"/>
-                </a>
-                <p class='titleColorTwo'>Whole Foods</p>
-                <p class='harvard'>Nature!</p>
-                <p class='titleColorTwo'>Integration!</p>
-            </figure>
-            <figure>
-                <figcaption><p class='titleColorTwo'>Evolution of Diet</p></figcaption>
-                <a href="https://www.nationalgeographic.com/foodfeatures/evolution-of-diet/" target="_blank">
-                    <img class="img8 aPng" src="images/evolutionOfDiet.jpg" alt="Humans eat what, where, how?"/>
-                </a>
-                <p class='titleColorTwo'>What?</p>
-                <p class='harvard'>Where?</p>
-                <p class='titleColorTwo'>How?</p>
-            </figure>
+                <h2 class='harvard'><a class="harvard" href="docs/whyMFEIsAVegan.jpg" target="_blank">*** Because ***</a></h2>
+                <figure>
+                    <figcaption><p class='titleColorTwo'>What the Health</p></figcaption>
+                    <a href="http://www.whatthehealthfilm.com/" target="_blank">
+                        <img class="img8 aPng" src="images/whatTheHealth.jpg"
+                        alt="What the Health: Direct or Netflix"/>
+                    </a>
+                    <p class='titleColorTwo'>Whole Foods</p>
+                    <p class='harvard'>No Meat</p>
+                    <p class='titleColorTwo'>No Milk</p>
+                </figure>
+                <figure>
+                    <figcaption><p class='titleColorTwo'>The Magic Pill</p></figcaption>
+                    <a href="https://www.netflix.com/title/80238655" target="_blank">
+                        <img class="img8 aPng" src="images/theMagicPill.jpg"
+                        alt="The Magic Pill: Netflix"/>
+                    </a>
+                    <p class='titleColorTwo'>Whole Foods</p>
+                    <p class='harvard'>No Milk</p>
+                    <p class='titleColorTwo'>Meat</p>
+                </figure>
+                <figure>
+                    <figcaption><p class='titleColorTwo'>The C Word</p></figcaption>
+                    <a href="http://thecwordmovie.com/" target="_blank">
+                        <img class="img8 aPng" src="images/theCWord.jpg"
+                        alt="The C Word: Direct or Netflix"/>
+                    </a>
+                    <p class='titleColorTwo'>Healthy Diet!</p>
+                    <p class='harvard'>Excercise</p>
+                    <p class='titleColorTwo'>No Toxin</p>
+                </figure>
+                <figure>
+                    <figcaption><p class='titleColorTwo'>In Search of Balance</p></figcaption>
+                    <a href="https://www.netflix.com/title/80164396" target="_blank">
+                        <img class="img8 aPng" src="images/inSearchOfBalance.jpg"
+                        alt="In Search of Balance Netflix"/>
+                    </a>
+                    <p class='titleColorTwo'>Whole Foods</p>
+                    <p class='harvard'>Nature!</p>
+                    <p class='titleColorTwo'>Integration!</p>
+                </figure>
+                <figure>
+                    <figcaption><p class='titleColorTwo'>Evolution of Diet</p></figcaption>
+                    <a href="https://www.nationalgeographic.com/foodfeatures/evolution-of-diet/" target="_blank">
+                        <img class="img8 aPng" src="images/evolutionOfDiet.jpg" alt="Humans eat what, where, how?"/>
+                    </a>
+                    <p class='titleColorTwo'>What?</p>
+                    <p class='harvard'>Where?</p>
+                    <p class='titleColorTwo'>How?</p>
+                </figure>
             </section>
             <!-- User Opinion -->
             <figure id="emailBox" class="displayNone">
